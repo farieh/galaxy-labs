c5bbaafd4781ac16d54c642f8faeb489c47d4d3e
farih <farieh87@yahoo.co.id>
2016-09-14 10:52:13 +0700

    delete function

--- moodle client/index.js
+++ moodle client/index.js
@@ -9,14 +9,32 @@ moodle_client.init({
 }).then(function (client) {
     console.log(client);
     //do_something(client);
-    do_otherthing(client);
+    //do_otherthing(client);
     get_all_user(client);
-
 
 }).catch(function (err) {
     console.log("Unable to initialize the client: " + err);
 });
 
+
+function delete_user(id) {
+    moodle_client.init({
+        wwwroot: "http://localhost/moodle/",
+        token: "e7c2eb02d743314e32b3764492004ef7",
+        service: "Administrator"
+
+    }).then(function (client) {
+
+        client.call({
+            wsfunction: 'core_user_delete_users',
+            args: {
+                userids: [id]
+            },
+        }).then(function (info) {
+            console.log('User '+id+' deleted');
+        })
+    })
+}
 
 function get_all_user(client) {
     client.call({
@@ -34,13 +52,15 @@ function get_all_user(client) {
         console.log(info);
         var arraylength = info.users.length;
         for (var i = 1; i < arraylength; i++) {
-            tableUser = tableUser + '<tr><td>' + info.users[i].id + '</td><td>' + info.users[i].username + '</td><td>' + info.users[i].fullname + '</td><td>' + info.users[i].email + '</td></tr>';
+            tableUser = tableUser + '<tr><td>' + info.users[i].id + '</td><td>' + info.users[i].username + '</td><td>' + info.users[i].fullname + '</td><td>' + info.users[i].email + '</td><td><a href="#" id="user-delete" onClick="delete_user('+info.users[i].id+')" value="' + info.users[i].id + '">Delete</a></td></tr>';
         };
 
         $("#table-body").append(tableUser);
     });
 }
 var tableUser;
+
+
 
 function do_otherthing(client) {
     client.call({
@@ -50,14 +70,14 @@ function do_otherthing(client) {
             users: [
                 {
                     username: 'Nunu',
-                    password: 'Nunu',
+                    password: 'Nanana',
                     createpassword: 0,  //True if password should be created and mailed to user.
                     firstname: 'Nunu',   //The first name(s) of the user
                     lastname: 'Nana',   //The family name of the user
                     email: 'nunu@gmail.com',//A valid and unique email address
                     auth: 'manual',//Auth plugins include manual, ldap, imap, etc
                     idnumber: '', //An arbitrary ID code number perhaps from the institution
-                    lang: 'eng',//Language code such as "en", must exist on server
+                    lang: 'en',//Language code such as "en", must exist on server
                     calendartype: 'gregorian', //Calendar type such as "gregorian", must exist on server
                     theme: 'standard', //Theme name such as "standard", must exist on server
                     timezone: '99',//Timezone code such as Australia/Perth, or 99 for default
