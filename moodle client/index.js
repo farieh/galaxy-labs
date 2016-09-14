35bf6ff12b2f393c7ae1e4fa58f6762b28aef2f7
farih <farieh87@yahoo.co.id>
2016-09-14 14:40:43 +0700

    update add registering user

--- moodle client/index.js
+++ moodle client/index.js
@@ -12,6 +12,7 @@ moodle_client.init({
     //do_otherthing(client);
     get_all_user(client);
 
+
 }).catch(function (err) {
     console.log("Unable to initialize the client: " + err);
 });
@@ -31,7 +32,7 @@ function delete_user(id) {
                 userids: [id]
             },
         }).then(function (info) {
-            console.log('User '+id+' deleted');
+            console.log('User ' + id + ' deleted');
         })
     })
 }
@@ -49,10 +50,10 @@ function get_all_user(client) {
 
         },
     }).then(function (info) {
-        console.log(info);
+        //console.log(info);
         var arraylength = info.users.length;
         for (var i = 1; i < arraylength; i++) {
-            tableUser = tableUser + '<tr><td>' + info.users[i].id + '</td><td>' + info.users[i].username + '</td><td>' + info.users[i].fullname + '</td><td>' + info.users[i].email + '</td><td><a href="#" id="user-delete" onClick="delete_user('+info.users[i].id+')" value="' + info.users[i].id + '">Delete</a></td></tr>';
+            tableUser = tableUser + '<tr><td>' + info.users[i].id + '</td><td>' + info.users[i].username + '</td><td>' + info.users[i].fullname + '</td><td>' + info.users[i].email + '</td><td><a href="#" id="user-delete" onClick="delete_user(' + info.users[i].id + ')" value="' + info.users[i].id + '">Delete</a></td></tr>';
         };
 
         $("#table-body").append(tableUser);
@@ -61,43 +62,16 @@ function get_all_user(client) {
 var tableUser;
 
 
-
 function do_otherthing(client) {
     client.call({
-        wsfunction: 'core_user_create_users',
+        wsfunction: 'core_course_create_categories',
         method: 'POST',
         args: {
-            users: [
-                {
-                    username: 'Nunu',
-                    password: 'Nanana',
-                    createpassword: 0,  //True if password should be created and mailed to user.
-                    firstname: 'Nunu',   //The first name(s) of the user
-                    lastname: 'Nana',   //The family name of the user
-                    email: 'nunu@gmail.com',//A valid and unique email address
-                    auth: 'manual',//Auth plugins include manual, ldap, imap, etc
-                    idnumber: '', //An arbitrary ID code number perhaps from the institution
-                    lang: 'en',//Language code such as "en", must exist on server
-                    calendartype: 'gregorian', //Calendar type such as "gregorian", must exist on server
-                    theme: 'standard', //Theme name such as "standard", must exist on server
-                    timezone: '99',//Timezone code such as Australia/Perth, or 99 for default
-                    mailformat: 1,//Mail format code is 0 for plain text, 1 for HTML etc
-                    description: 'Profile Desciption', //User profile description, no HTML
-                    city: 'Kota', //Home city of the user
-                    country: 'AU', //Home country code of the user, such as AU or CZ
-                    firstnamephonetic: 'as',//The first name(s) phonetically of the user
-                    lastnamephonetic: 'asd',//The family name phonetically of the user
-                    middlename: 'asdasd', //The middle name of the user
-                    alternatename: 'asdasdas',//The alternate name of the user
-                    preferences: [{
-                        type: 'string',  //The name of the preference
-                        value: 'string'   //The value of the preference
-                    }], //User preferences
-                    customfields: [{
-                        type: 'string',
-                        value: 'string'
-                    }]
-                }]
+            categories: [{
+                name: 'Pendidikan',
+                parent: 0,
+                descriptionformat: 1
+            }]
 
         },
     }).then(function (info) {
@@ -108,11 +82,53 @@ function do_otherthing(client) {
 
 function do_something(client) {
     client.call({
-        wsfunction: "core_webservice_get_site_info",
-
+        wsfunction: "core_user_create_users",
+        method: 'POST',
+        args: {
+            users: [{
+                username: 'nani',
+                password: 'B1smillah..',
+                firstname: 'Nuna',
+                lastname: 'Narita',
+                email: 'nani@gmail.com',
+                auth: 'manual',
+                idnumber: '',
+                lang: 'en',
+                calendartype: 'gregorian'
+            }]
+        },
     }).then(function (info) {
         console.log(info);
-        console.log("Hello %s, welcome to %s", info.fullname, info.sitename);
+
     });
 }
 
+
+function create_user(client,usrname,psword,fsname,lsname,usrmail) {
+    client.call({
+        wsfunction: "core_user_create_users",
+        method: 'POST',
+        args: {
+            users: [{
+                username: usrname,
+                password: psword,
+                firstname: fsname,
+                lastname: lsname,
+                email: usrmail,
+                auth: 'manual',
+                idnumber: '',
+                lang: 'en',
+                calendartype: 'gregorian'
+            }]
+        },
+    }).then(function (info) {
+        console.log(info);
+        
+        if(info[0].username != null){
+            console.log('Complete registering '+usrname)
+            $("#create-user").collapse('hide');
+            $("input").val('');
+        }
+        
+    });
+}
